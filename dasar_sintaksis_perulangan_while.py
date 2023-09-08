c52f492fe246e2d4340f6dcb8d02baf0f6622901
Husaen al Fatah <husaenaf26@gmail.com>
2023-09-08 09:39:38 +0700

    its work

--- dasar_sintaksis_perulangan_while.py
+++ dasar_sintaksis_perulangan_while.py
@@ -6,8 +6,11 @@ jumlah_buku = 10
 print('ibu berkata, "baca semua bukumu"')
 
 jumlah_buku_yang_sudah_dibaca = 0
+print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
 
-while jumlah_buku_yang_sudah_dibaca < 10:
+while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
     jumlah_buku_yang_sudah_dibaca +=1
+    #arti kode diatas adalah jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
     print(f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
 print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
